1e4ad0406dc1850f3352e0a9401357dc56746dad
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-09-16 01:44:52 +0300

    Navbar and sidebar files are added, routing is not added yet

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { Navbar } from "./components";
+
 const App = () => {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">
-        Hello world!
+        <Navbar />
       </h1>
     </div>
   )
---
+++ src/components/Common/Navbar.tsx
@@ -0,0 +1,13 @@
+import { FiMenu } from 'react-icons/fi';
+import { RiLayoutMasonryFill } from 'react-icons/ri';
+import { FaFolderOpen } from 'react-icons/fa';
+
+const Navbar = () => {
+  return (
+    <nav className='fixed z-10 top-0 right-0 left-0 h-12 bg-zinc-800'>
+      {/* My navbar (Went to sleep... Zzzzz...) */}
+    </nav>
+  )
+}
+
+export default Navbar
