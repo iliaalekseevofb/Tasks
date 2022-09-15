824ef2de2e8d71591d68822b3a146e285cee3a6f
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-09-15 21:45:38 +0300

    Necessary dependencies are installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 const App = () => {
   return (
-    <div>App</div>
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
   )
 }
 
