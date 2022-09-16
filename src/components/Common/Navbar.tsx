8538ae928ff2f5859a52af6e8e715f2d8d9ef97a
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-09-16 14:37:03 +0300

    Navbar's left section is partially done (not functional, not responsive)

--- src/components/Common/Navbar.tsx
+++ src/components/Common/Navbar.tsx
@@ -4,8 +4,23 @@ import { FaFolderOpen } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
-    <nav className='fixed z-10 top-0 right-0 left-0 h-12 bg-zinc-800'>
-      {/* My navbar (Went to sleep... Zzzzz...) */}
+    <nav className='fixed z-10 top-0 right-0 left-0 h-12 px-6 flex justify-between items-center bg-secondaryBg'>
+      <section className='flex items-center text-iconsColor text-sm font-semibold'>
+        <button className='p-1 rounded-lg mr-4 hover:bg-secondaryBgHover transition-colors duration-200'>
+          <FiMenu size={24} />
+        </button>
+        <button className='py-1 px-2 rounded-lg mr-4 flex items-center hover:bg-secondaryBgHover transition-colors duration-200'>
+          <RiLayoutMasonryFill className='mr-1' size={22} />
+          <h6>Dashboard</h6>
+        </button>
+        <button className='py-1 px-2 rounded-lg flex items-center hover:bg-secondaryBgHover transition-colors duration-200'>
+          <FaFolderOpen className='mr-1' size={22} />
+          <h6>Collections</h6>
+        </button>
+      </section>
+      <section>
+
+      </section>
     </nav>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mainBg: '#1e1d26',
+        secondaryBg: '#282730',
+        secondaryBgHover: '#494854',
+        iconsColor: '#b4b2c2'
+      }
+    },
   },
   plugins: [],
 }
