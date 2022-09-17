54f32ddfaf0f6bf6a894e98b5f31501e1b931a61
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-09-17 16:50:51 +0300

    Sidebar is closing reaching the certain size of the screen

--- src/components/Common/Sidebar.tsx
+++ src/components/Common/Sidebar.tsx
@@ -6,7 +6,7 @@ const Sidebar = () => {
   const {collections, isOpenSidebar} = useContext(GlobalContext);
 
   return (
-    <div className={`fixed z-10 top-14 left-0 bottom-0 w-64 h-screen py-6 ${isOpenSidebar ? 'translate-x-0' : '-translate-x-full'} ease-in-out duration-200 border-r-2 border-secondaryBgHover bg-secondaryBgHover bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-20`}>
+    <div className={`fixed z-10 top-14 left-0 bottom-0 w-64 h-screen py-6 ${isOpenSidebar ? '-translate-x-full sm:translate-x-0' : '-translate-x-full'} ease-in-out duration-200 border-r-2 border-secondaryBgHover bg-secondaryBgHover bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-20`}>
       <h4 className="mb-3 px-standardP text-lg font-bold text-standardC">Collections</h4>
       {collections.map((item) => (
         <Link key={item.id} className='w-full py-4 px-standardP flex items-center hover:bg-secondaryBgHover transition-colors duration-200 cursor-pointer' to={`collections/${item.link}`}>
